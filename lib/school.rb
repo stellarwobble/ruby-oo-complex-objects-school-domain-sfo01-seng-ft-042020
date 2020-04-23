51ab6b0540f1e65e1ec9c28edb5389f025072a90
stellarwobble <jennidemuir@gmail.com>
2020-04-22 17:37:39 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+    attr_accessor :roster, :school, :student
+
+    def initialize(school)
+        @school = school
+        @roster = Hash.new
+       end 
+
+       def add_student(name, grade)
+        if @roster[grade] == nil
+           @roster[grade] = []
+        end 
+        @roster[grade]<< name
+       end 
+       
+       def grade(level) 
+        @roster.select do |k,v|
+            if k == level
+                return v
+            end 
+        end 
+    end 
+
+    def sort
+        nu_hash = Hash.new
+        @roster.each do |x,y|
+         nu_hash[x] = y.sort
+        end 
+        nu_hash
+     end 
+
+end 
